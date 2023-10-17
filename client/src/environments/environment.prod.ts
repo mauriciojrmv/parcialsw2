5c06b609a544573d786a10d6016b992aaba76af4
Mauricio Mattinen <mauricio.jr.mattinen.valdez@gmail.com>
2023-10-17 15:13:12 -0400

    update

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://parcialsw2/api',
-  socketUrl: 'http://parcialsw2',
+  apiUrl: 'http://localhost:4001/api',
+  socketUrl: 'http://localhost:4001',
 };
